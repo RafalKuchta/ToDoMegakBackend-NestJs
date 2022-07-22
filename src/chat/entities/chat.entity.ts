42cb6f8339eeea1247b0cb2a71ff233d6c1784e1
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-07-22 11:19:20 +0200

    Added sort messages by created_at: "ASC" in Chat entity.

--- src/chat/entities/chat.entity.ts
+++ src/chat/entities/chat.entity.ts
@@ -1,1 +1,28 @@
-export class Chat {}
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity({
+    orderBy: {
+        created_at: "ASC",
+    }
+})
+export class Chat extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        length: 99999
+    })
+    message: string;
+
+    @Column({
+        length: 255,
+    })
+    user: string;
+
+    @Column({
+        type: 'datetime',
+        default: () => 'NOW()',
+    })
+    created_at: Date;
+
+}
