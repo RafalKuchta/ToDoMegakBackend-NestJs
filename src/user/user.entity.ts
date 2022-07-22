d084033b5556d8da96a0406fb3887ba256d8f6c5
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-07-22 11:20:32 +0200

    Cleaned code.

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,4 @@
-import {BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {Task} from "../tasks/entities/task.entity";
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
 export class User extends BaseEntity {
