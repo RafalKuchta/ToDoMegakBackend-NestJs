d382155f4d976fe096e35798682be94b60c6e010
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-09-02 11:31:54 +0200

    Added function send sms to group users

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,66 +1,176 @@
 import {Injectable} from '@nestjs/common';
 import {CreateSmDto, CreateSmsDto} from './dto/create-sm.dto';
-import {UpdateSmDto} from './dto/update-sm.dto';
-import {AddSmsResponse, SendSmsResponse} from "../interfaces/sms";
-import {Sm} from "./entities/sm.entity";
-import {User} from "../user/user.entity";
+
+import {
+    AddGroupResponse,
+    AddSmsResponse, SendGroupResponse, SendResponse
+} from "../interfaces/sms";
+import {Phone} from "./entities/phone.entity";
 import {Sms} from "./entities/sms.entity";
+import {Group} from "./entities/group.entity";
+
 
 @Injectable()
 export class SmsService {
 
-    filter(sm: Sm): AddSmsResponse {
-        const {id, phone, name, surname, company} = sm;
-        return {id, phone, name, surname, company};
+    filter(phoneToAdd: Phone): AddSmsResponse {
+        const {id, phone, name, surname, company, position, groups} = phoneToAdd;
+        return {id, phone, name, surname, company, position, groups};
+    }
+
+    filterGroup(groupToAdd: Group): AddGroupResponse {
+        const {id, name} = groupToAdd;
+        return {id, name};
     }
 
 
     async create(createSmDto: CreateSmDto): Promise<AddSmsResponse> {
-        const sm = new Sm();
-        sm.name = createSmDto.name;
-        sm.surname = createSmDto.surname;
-        sm.company = createSmDto.company;
-        sm.phone = createSmDto.phone;
-        sm.group = createSmDto.group;
+        const phone = new Phone();
+        phone.name = createSmDto.name;
+        phone.surname = createSmDto.surname;
+        phone.company = createSmDto.company;
+        phone.position = createSmDto.position;
+        phone.phone = createSmDto.phone;
 
-        await sm.save();
+        await phone.save();
 
-        return this.filter(sm);
+        return this.filter(phone);
     }
 
-    async send(createSmsDto: CreateSmsDto): Promise<SendSmsResponse> {
-        console.log(createSmsDto)
+    async send(createSmsDto: CreateSmsDto): Promise<SendResponse> {
         const sms = new Sms();
         sms.phone = createSmsDto.mobile_number;
-        sms.group = createSmsDto.mobile_numbers;
         sms.sms = createSmsDto.message;
 
-        await sms.save();
-
-        for (const item of createSmsDto.phones){
-            const sms2 = new Sms();
-                    sms2.phone = item.phone;
-                    sms2.sms = createSmsDto.message;
-
-                    await sms2.save()
+        if (createSmsDto.mobile_number.length > 5) {
+            await sms.save();
         }
 
-        return sms;
+        if (createSmsDto.phones) {
+            for (const item of createSmsDto.phones) {
+                const sms2 = new Sms();
+                sms2.phone = '+48' + item.phone;
+                sms2.sms = createSmsDto.message;
+
+                await sms2.save()
+            }
+            return {
+                message: 'ok',
+            };
+        }
+
+        if (createSmsDto.groups) {
+            for (const item of createSmsDto.groups) {
+                if (item.group === 'Próbkobiorcy') {
+                    const allProbkoPhones = await Phone.find();
+
+                    for (const allphones of allProbkoPhones) {
+                        const smsToAll = new Sms();
+                        smsToAll.phone = '+48' + allphones.phone;
+                        smsToAll.name = allphones.name;
+                        smsToAll.surname = allphones.surname;
+                        smsToAll.company = allphones.company;
+                        smsToAll.group = item.group;
+                        smsToAll.sms = createSmsDto.message;
+                        await smsToAll.save()
+                    }
+
+                }
+
+                const phonesFromGroups = await Phone.find({
+                    where: {
+                        company: item.group,
+                    }
+                });
+
+                for (const phonesFromGroup of phonesFromGroups) {
+                    const smsToGroup = new Sms();
+                    smsToGroup.phone = '+48' + phonesFromGroup.phone;
+                    smsToGroup.name = phonesFromGroup.name;
+                    smsToGroup.surname = phonesFromGroup.surname;
+                    smsToGroup.company = phonesFromGroup.company;
+                    smsToGroup.group = item.group;
+                    smsToGroup.sms = createSmsDto.message;
+                    await smsToGroup.save()
+                }
+
+            }
+        }
+        return {
+            message: 'ok',
+        };
+
     }
 
-    async findAll(user: User): Promise<AddSmsResponse[]> {
-        return (await Sm.find()).map(this.filter);
+    async findAll(): Promise<AddSmsResponse[]> {
+        return (await Phone.find({
+            relations: ['groups'],
+        })).map(this.filter);
+
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} sm`;
+    async findAllGroups(): Promise<SendGroupResponse[]> {
+        return (await Group.find()).map(this.filterGroup);
     }
 
-    update(id: number, updateSmDto: UpdateSmDto) {
-        return `This action updates a #${id} sm`;
+    async findOneNumber(id: string): Promise<Phone> {
+        return await Phone.findOne({
+            where: {
+                id,
+            },
+            relations: {
+                groups: true,
+            },
+        });
+    }
+
+    async update(id: string, req: CreateSmDto) {
+        if (!id) {
+            throw new Error('Task not found!')
+        }
+
+        const phone = await Phone.findOne({
+            where: {
+                id,
+            }
+        });
+
+        if (phone) {
+            phone.name = req.name;
+            phone.surname = req.surname;
+            phone.company = req.company;
+            phone.position = req.position;
+            phone.phone = req.phone;
+            await phone.save();
+
+            if (req.group) {
+                const group = await Group.findOne({
+                    where: {
+                        name: req.group,
+                    },
+                });
+
+                phone.groups = group;
+                await phone.save();
+            } else {
+                phone.groups = null;
+                await phone.save();
+            }
+
+            return {
+                isSuccess: true,
+            };
+        }
+
+        return {
+            isSuccess: false,
+        };
+
     }
 
     remove(id: number) {
         return `This action removes a #${id} sm`;
     }
+
+
 }
