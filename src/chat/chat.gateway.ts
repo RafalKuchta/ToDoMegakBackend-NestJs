29a77dcf07ac6cc81676f7af1f2ed628bcb00133
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-09-13 15:18:30 +0200

    Removed console.log

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,6 @@
 import {MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
 import {UserObj} from "../decorators/user-obj.decorator";
 import {User} from "../user/user.entity";
-import {ChatService} from "./chat.service";
 
 @WebSocketGateway(3002, {cors: '*'})
 export class ChatGateway {
@@ -13,7 +12,6 @@ export class ChatGateway {
       @MessageBody() message: string,
       @UserObj() user: User,
   ): Promise<void> {
-    console.log(message, user);
     this.server.emit('message', message);
   }
 }
