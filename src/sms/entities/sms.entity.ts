48831d31abcac85188a9c01b29965fc5840300f0
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-10-18 11:53:16 +0200

    Added user column

--- src/sms/entities/sms.entity.ts
+++ src/sms/entities/sms.entity.ts
@@ -1,53 +1,56 @@
-import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Sms extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        length: 255,
-        default: null,
-        nullable: true,
-    })
-    name: string;
+  @Column({
+    length: 255,
+    default: null,
+    nullable: true,
+  })
+  name: string;
 
-    @Column({
-        length: 255,
-        default: null,
-        nullable: true,
-    })
-    surname: string;
+  @Column({
+    length: 255,
+    default: null,
+    nullable: true,
+  })
+  surname: string;
 
-    @Column({
-        length: 255,
-        default: null,
-        nullable: true,
-    })
-    company: string;
+  @Column({
+    length: 255,
+    default: null,
+    nullable: true,
+  })
+  company: string;
 
-    @Column({
-        length: 15,
-        default: null,
-    })
-    phone: string;
+  @Column({
+    length: 15,
+    default: null,
+  })
+  phone: string;
 
-    @Column({
-        length: 255,
-        default: null,
-        nullable: true,
-    })
-    group: string;
+  @Column({
+    length: 255,
+    default: null,
+    nullable: true,
+  })
+  group: string;
 
-    @Column({
-        length: 255,
-    })
-    sms: string;
+  @Column({
+    length: 255,
+  })
+  sms: string;
 
-    @Column({
-        type: 'datetime',
-        default: () => 'NOW()',
-    })
-    created_at: string;
+  @Column({
+    length: 255,
+  })
+  user: string;
 
+  @Column({
+    default: () => 'NOW()',
+  })
+  created_at: string;
 }
