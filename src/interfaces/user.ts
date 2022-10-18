b89bc1bf3749339842d7b8f094c878717c16fc64
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-10-18 11:57:29 +0200

    Edited user services

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,5 +1,15 @@
 export interface RegisterUserResponse {
-    id: string;
-    email: string;
-    roles: [];
+  id: string;
+  email: string;
+  roles: [];
+}
+
+export interface UserResponse {
+  id: string;
+  email: string;
+  roles: string;
+}
+
+export interface UserRolesResponse {
+  roles: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,45 +1,85 @@
-import {Inject, Injectable} from "@nestjs/common";
-import {RegisterDto} from "./dto/register.dto";
-import {RegisterUserResponse} from "../interfaces/user";
-import {User} from "./user.entity";
-import {hashPwd} from "../utils/hash-pwd";
-import {MailService} from "../mail/mail.service";
-import {registeredNewUserInfo} from "../templates/email/registered-new-user-info";
+import { Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import {
+  RegisterUserResponse,
+  UserResponse,
+  UserRolesResponse,
+} from '../interfaces/user';
+import { User } from './user.entity';
+import { hashPwd } from '../utils/hash-pwd';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from './roles.entity';
 
 @Injectable()
 export class UserService {
-    constructor(
-      @Inject(MailService) private mailService: MailService,
-    ) {}
-
   filter(user: User): RegisterUserResponse {
-    const {id, email, roles} = user;
-    return {id, email, roles:[]};
+    const { id, email, roles } = user;
+    return { id, email, roles: [] };
+  }
+
+  filterGetUsers(user: User): UserResponse {
+    const { id, email, roles } = user;
+    return { id, email, roles };
   }
 
   async register(newUser: RegisterDto): Promise<RegisterUserResponse> {
-      const user = new User();
-      user.email = newUser.email;
-      user.pwdHash = hashPwd(newUser.pwd);
+    const user = new User();
+    user.email = newUser.email;
+    user.pwdHash = hashPwd(newUser.pwd);
 
-      // const findUserinBase = await User.findOne({where: {
-      //         email: newUser.email,
-      //     }});
-      //
-      // if (findUserinBase.email){
-      //     throw new ConflictException(`Username: ${findUserinBase.email} is existed`)
-      // }
+    // const findUserinBase = await User.findOne({where: {
+    //         email: newUser.email,
+    //     }});
+    //
+    // if (findUserinBase.email){
+    //     throw new ConflictException(`Username: ${findUserinBase.email} is existed`)
+    // }
 
-          await user.save();
-
-          await this.mailService.sendMail('r.kuchta@gba-polska.pl', 'Rejestracja użytkownika.', registeredNewUserInfo(user.email))
-
-          return this.filter(user);
-
+    await user.save();
+    return this.filter(user);
   }
 
   async getOneUser(id: string): Promise<User> {
-      return await User.findOne({where: {id}});
+    const response = await User.findOne({
+      where: { id },
+    });
+
+    return response;
   }
 
+  async getAll(): Promise<UserResponse[]> {
+    const response: UserResponse[] = await User.find();
+
+    return response.map(this.filterGetUsers);
+  }
+
+  async update(id: string, req: UpdateUserDto) {
+    if (!id) {
+      throw new Error('Task not found!');
+    }
+
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (user) {
+      user.email = req.email;
+      user.roles = req.roles;
+      await user.save();
+
+      return {
+        isSuccess: true,
+      };
+    }
+
+    return {
+      isSuccess: false,
+    };
+  }
+
+  async findAllRoles(): Promise<UserRolesResponse[]> {
+    return await Roles.find();
+  }
 }
