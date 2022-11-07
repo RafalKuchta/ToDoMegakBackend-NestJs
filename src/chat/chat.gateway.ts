657459af947d5e29d139f661e1c0d1a70c8fe730
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-11-07 13:50:43 +0100

    Edited cors port.

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,16 +1,23 @@
-import {MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
-import {UserObj} from "../decorators/user-obj.decorator";
-import {User} from "../user/user.entity";
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/user.entity';
 
-@WebSocketGateway(3002, {cors: '*'})
+@WebSocketGateway({
+  cors: ['*'],
+})
 export class ChatGateway {
-
   @WebSocketServer()
   server;
   @SubscribeMessage('message')
   async handleMessage(
-      @MessageBody() message: string,
-      @UserObj() user: User,
+    @MessageBody() message: string,
+    @UserObj() user: User,
   ): Promise<void> {
     this.server.emit('message', message);
   }
