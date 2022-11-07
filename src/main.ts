fb963c6850f1e5cda5cc4e1ffcba6bd8b98c1091
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-11-07 13:52:33 +0100

    Edited cors data.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { whitelistCors } from './utils/config/cors-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('app');
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
+    origin: whitelistCors.address,
+    methods: whitelistCors.methods,
     credentials: true,
+    optionsSuccessStatus: 200,
   });
 
   app.use(cookieParser());
 
   await app.listen(3001, () => {
-    console.log('http://localhost:3001');
+    console.log("http://localhost:3001")
   });
 }
-bootstrap();
+(async () => {
+  await bootstrap();
+})();
